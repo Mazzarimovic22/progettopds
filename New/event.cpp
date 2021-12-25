a63df17e68bda2fec678b1f27f6ff47986ba2406
Mazzarimovic22 <mattiamazzari@gmail.com>
2021-12-25 11:32:24 +0100

    Second Mattia commit

--- New/event.cpp
+++
@@ -1,7 +0,0 @@
-#include "event.h"
-
-Event::Event(QString uid, QString summary, QString location, QString date, QString dtstart, QString dtend):
-uid(uid), summary(summary), location(location), date(date), dtstart(dtstart), dtend(dtend)
-{
-
-}
